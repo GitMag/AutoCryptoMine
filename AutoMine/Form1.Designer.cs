5b4b12fc9f09273b6eab94e3b70117eda98d8ed7
MagGit <magnus.forsblom@gmail.com>
2017-12-09 17:33:02 +0200

    Intitial release.

---
+++ AutoMine/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace AutoMine
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DialogButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.MinerLocationDialog = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StartWithWindowsCheckBox = new System.Windows.Forms.CheckBox();
+            this.MinerTextBox = new System.Windows.Forms.TextBox();
+            this.InactivityTimeTextBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Start mining after";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(200, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "seconds";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "of inactivity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Miner location:";
+            // 
+            // DialogButton
+            // 
+            this.DialogButton.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DialogButton.Location = new System.Drawing.Point(231, 97);
+            this.DialogButton.Name = "DialogButton";
+            this.DialogButton.Size = new System.Drawing.Size(43, 31);
+            this.DialogButton.TabIndex = 6;
+            this.DialogButton.Text = "...";
+            this.DialogButton.UseVisualStyleBackColor = true;
+            this.DialogButton.Click += new System.EventHandler(this.DialogButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SaveButton.Location = new System.Drawing.Point(160, 135);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(114, 31);
+            this.SaveButton.TabIndex = 8;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(117, 48);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // MinerLocationDialog
+            // 
+            this.MinerLocationDialog.FileName = "openFileDialog1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StartWithWindowsCheckBox
+            // 
+            this.StartWithWindowsCheckBox.AutoSize = true;
+            this.StartWithWindowsCheckBox.Checked = global::AutoMine.Properties.Settings.Default.StartWithWindowsVALUE;
+            this.StartWithWindowsCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::AutoMine.Properties.Settings.Default, "StartWithWindowsVALUE", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.StartWithWindowsCheckBox.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartWithWindowsCheckBox.Location = new System.Drawing.Point(17, 143);
+            this.StartWithWindowsCheckBox.Name = "StartWithWindowsCheckBox";
+            this.StartWithWindowsCheckBox.Size = new System.Drawing.Size(135, 24);
+            this.StartWithWindowsCheckBox.TabIndex = 7;
+            this.StartWithWindowsCheckBox.Text = "Start with windows";
+            this.StartWithWindowsCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // MinerTextBox
+            // 
+            this.MinerTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::AutoMine.Properties.Settings.Default, "MinerLocationVALUE", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.MinerTextBox.Font = new System.Drawing.Font("Arial Narrow", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MinerTextBox.Location = new System.Drawing.Point(17, 97);
+            this.MinerTextBox.Name = "MinerTextBox";
+            this.MinerTextBox.Size = new System.Drawing.Size(207, 32);
+            this.MinerTextBox.TabIndex = 5;
+            this.MinerTextBox.Text = global::AutoMine.Properties.Settings.Default.MinerLocationVALUE;
+            // 
+            // InactivityTimeTextBox
+            // 
+            this.InactivityTimeTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::AutoMine.Properties.Settings.Default, "InactivityTimeVALUE", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.InactivityTimeTextBox.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InactivityTimeTextBox.Location = new System.Drawing.Point(160, 6);
+            this.InactivityTimeTextBox.Name = "InactivityTimeTextBox";
+            this.InactivityTimeTextBox.Size = new System.Drawing.Size(40, 29);
+            this.InactivityTimeTextBox.TabIndex = 1;
+            this.InactivityTimeTextBox.Text = global::AutoMine.Properties.Settings.Default.InactivityTimeVALUE;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(284, 181);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.StartWithWindowsCheckBox);
+            this.Controls.Add(this.DialogButton);
+            this.Controls.Add(this.MinerTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InactivityTimeTextBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "AutoMine Settings";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InactivityTimeTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox MinerTextBox;
+        private System.Windows.Forms.Button DialogButton;
+        private System.Windows.Forms.CheckBox StartWithWindowsCheckBox;
+        private System.Windows.Forms.Button SaveButton;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.OpenFileDialog MinerLocationDialog;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ AutoMine/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace AutoMine
+{
+    public partial class MainForm : Form
+    {
+        int MinuteAmount = 0; //variable to store how many seconds is needed to open miner
+        int oldXloc = 0; // old location of x mouse
+        int newXloc = 0; // new location of x mouse
+        string noextnesion = ""; //Miner filename whitout extension
+        bool allowexit = false;
+        public MainForm()
+          
+
+        {
+            InitializeComponent();
+        }
+        
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.Save(); // Saves settings in config file
+            this.Hide();
+        }
+
+        private void DialogButton_Click(object sender, EventArgs e)
+        {
+            if //Get file to textbox
+             (MinerLocationDialog.ShowDialog() == DialogResult.OK)
+            {
+                MinerTextBox.Text = MinerLocationDialog.FileName; // set textbox to display selected program
+                noextnesion = Path.GetFileNameWithoutExtension(MinerTextBox.Text); // get filename whitout extension .exe
+                Properties.Settings.Default["AppName"] = noextnesion; //store name in settings             
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //runs every 60 seconds
+            newXloc = Cursor.Position.X; // get current location
+            Console.WriteLine("NewXloc=" + newXloc.ToString() + "OldXLoc=" + oldXloc.ToString());
+            if (newXloc == oldXloc)
+            {
+                MinuteAmount++; //increment by 1 
+                
+                if (MinuteAmount.ToString() == InactivityTimeTextBox.Text) //check if target reached
+                {
+                    StartMiner(); // tartget reached, start miner
+                }
+            }
+            else
+            {
+                MinuteAmount = 0; // set MinuteAmount to zero because movement was detected
+                KillMiner(); //kill Miner if it is running
+                Console.WriteLine("MinuteAmount Reset");
+            }
+            oldXloc = Cursor.Position.X; //old location
+        }
+        public void StartMiner()
+        {
+           Process.Start(MinerTextBox.Text); //start process from MinerLocation
+        }
+        public void KillMiner()
+        {
+            Console.WriteLine("Attempting to kill " + Properties.Settings.Default["AppName"].ToString());
+            Process[] processList = Process.GetProcessesByName(Properties.Settings.Default["AppName"].ToString()); //get process name from settings
+
+            if (processList.Length > 0)
+            {
+                processList[0].Kill(); //kill process
+            }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        private void MainForm_Closing(object sender, CancelEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure want to exit the App?", "Test", MessageBoxButtons.YesNo) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
+
